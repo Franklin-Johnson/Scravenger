e4e637be42875f2da32a855b6bbbc9bf79a1ce4f
Aaron Sterzenback <Aaron.Sterzenback@my.utsa.edu>
2021-10-07 00:39:19 -0500

    new addition from desktop

--- EgitTesting/src/HelloWorld.java
+++ EgitTesting/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("New line is being added");
 		System.out.println("This is new feature addition.");
 		System.out.println("This is the first self-added line in new-feature.");
+		System.out.println("This is a line from desktop added on Wed 10/06");
 	}
 
 }
